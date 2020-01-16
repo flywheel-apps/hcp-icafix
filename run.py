e29a45ec9c89212e9966b5c5996d634f7ffbf6f3
davidparker1 <davidparker@flywheel.io>
2020-01-16 17:58:14 -0500

    Dev Push

--- run.py
+++ run.py
@@ -129,7 +129,7 @@ def check_input_files(workdir, zip_files):
 
     # HCP expects the functional file to have the same name as it's parent directory (MNINonLinear/Results/func_scan/func_scan.nii.gz)
     func_base = matches[0]
-    func_file = pathlib.PurePath(base).joinpath('MNINonLinear', 'Results', 'func_base','{}.nii.gz'.format(func_base))
+    func_file = pathlib.PurePath(base).joinpath('MNINonLinear', 'Results', func_base,'{}.nii.gz'.format(func_base))
     print('TRYING TO FIUCKING LOG')
     log.info("TRYING TO FUCKING LOG")
     log.info(zip_files[0])
