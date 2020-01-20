3e579126495d0fa25cc4789a465a2caf5225cff9
davidparker1 <davidparker@flywheel.io>
2020-01-20 11:28:20 -0500

    Dev Push

---
+++ test_icafix.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import os
+from gear_utils import gear_preliminaries as gp
+
+# tfMRI:
+# rfMRI_REST1_RL + LR
+# rfMRI_REST2_RL + LR
+# tfMRI_WM_RL + LR
+# tfMRI_GAMBLING_RL + LR
+# tfMRI_MOTOR_RL + LR
+# tfMRI_LANGUAGE_RL + LR
+# tfMRI_SOCIAL_RL + LR
+# tfMRI_RELATIONAL_RL + LR
+# tfMRI_EMOTION_RL + LR
+# 7 TASKS, 2 RESTING,  18 SCANS
+
+# 1 HCP pipelines only work with fsl 6
+# 2 use xenial vs trusty
+#
+
+struct_zip = '/flywheel/v0/input/TOME_3024_hcpstruct.zip'
+func_zip = '/flywheel/v0/input/TOME_3024_rfMRI_REST1_RL_hcpfunc.zip'
+
+
+
+
+cmd='${HCPPIPEDIR}/ICAFIX/hcp_fix /flywheel/v0/work/TOME_3024/MNINonLinear/Results/rfMRI_REST1_RL/rfMRI_REST1_RL.nii.gz 2000 TRUE HCP_hp2000.RData 10'
+work_dir = '/flywheel/v0/work'
+if not os.path.exists(work_dir):
+    os.mkdir(work_dir)
+    
+zip_file_list, config = gp.preprocess_hcp_zip(struct_zip)
+
+gp.unzip_hcp(False,work_dir,struct_zip)
+gp.unzip_hcp(False,work_dir,func_zip)
+
